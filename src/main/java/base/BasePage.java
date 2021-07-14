5ed44eb86e1719b533892b0fab2fa7e9d0383879
Sneha Agrawal <snehaagrawal@Snehas-MacBook-Air.local>
2021-07-14 20:31:40 +0530

    updates to FOrmData

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -4,12 +4,10 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 import supplier.DriverFactory;
 
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -18,31 +16,31 @@ public class BasePage {
     protected static WebDriver driver;
     public static Logger log = Logger.getLogger("devpinoyLogger");
 
-    @Parameters({"browserName"})
-    @BeforeTest
-    public void getDriver(@Optional("chrome") String browserName) {
+    @Parameters({"browserName" ,"platform"})
+    @BeforeMethod
+    public void getDriver(@Optional String browser, @Optional String platformName, Method method) {
 
-        MutableCapabilities sauceOpts = new MutableCapabilities();
-//        sauceOpts.setCapability("seleniumVersion", "3.141.59");
-        sauceOpts.setCapability("username", "Sneha_Kumari");
-        sauceOpts.setCapability("accessKey", "feb1dd07-2f58-4f63-b9e6-24f3593a7f63");
-//        sauceOpts.setCapability("build","Java-W3C-Examples");
-//        sauceOpts.setCapability("tags","w3c-chrome-tests");
+        String methodName = method.getName();
 
-        DesiredCapabilities dc = new DesiredCapabilities();
-        dc.setCapability("sauce:options", sauceOpts);
-//        dc.setCapability("browserVersion", "91");
-        dc.setCapability("platformName", "windows 10");
+//        sauceLab configuration
+//        MutableCapabilities sauceOpts = new MutableCapabilities();
+//        sauceOpts.setCapability("username", "Sneha_Kumari");
+//        sauceOpts.setCapability("accessKey", "feb1dd07-2f58-4f63-b9e6-24f3593a7f63");
+//        sauceOpts.setCapability("name",methodName);
+//
+//        DesiredCapabilities dc = new DesiredCapabilities();
+//        dc.setCapability("sauce:options", sauceOpts);
+//        dc.setCapability("platformName", "windows 10");
+//        dc.setCapability("browserName", browser);
 
-//        driver = DriverFactory.setUpDriver(browserName);
-        dc.setCapability("browserName", browserName);
-//        driver.manage().window().maximize();
+//        try {
+//            driver = new RemoteWebDriver(new URL("https://ondemand.apac-southeast-1.saucelabs.com:443/wd/hub"), dc);
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
 
-        try {
-            driver = new RemoteWebDriver(new URL("https://ondemand.apac-southeast-1.saucelabs.com:443/wd/hub"), dc);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        driver = DriverFactory.setUpDriver(browser);
+        driver.manage().window().maximize();
     }
 
     @AfterTest
@@ -54,7 +52,6 @@ public class BasePage {
 
         element.click();
         log.debug("Clicking on an Element : " + element);
-//        test.log(LogStatus.INFO, "Clicking on : " + element);
     }
 
     public void safeJavaScriptClick(WebElement element) throws Exception {
